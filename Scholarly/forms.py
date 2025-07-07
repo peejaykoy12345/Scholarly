78c0c6b137342bcb2ac83baf4ae17ff3eeede597
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-07 19:41:34 +0800

    Added the login, registration, and account form

---
+++ Scholarly/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
+from flask_wtf.file import FileAllowed
+from wtforms.validators import DataRequired, length,EqualTo, ValidationError, Email
+from Scholarly.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), length(min=2, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password =  PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken')
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is taken')
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password =  PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+
+class AccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), length(min=2, max=15)])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
