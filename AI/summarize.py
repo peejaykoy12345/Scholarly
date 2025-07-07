8b52acb66dc3384bac41b98e6b379210a0fa8e5d
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-07 19:54:57 +0800

    Made the summarized model

---
+++ AI/summarize.py
@@ -0,0 +1,37 @@
+from transformers.pipelines import pipeline
+
+summarize = pipeline("summarization", model="facebook/bart-large-cnn")
+
+def summarize_text(text: str) -> str:
+    if not text or len(text.strip()) == 0:
+        return "No input provided"
+    try:
+        return summarize(text)[0]['summary_text']
+    except Exception as e:
+        return f"Error summarizing: {str(e)}"
+    
+def split_text(text, max_chars=1000):
+    chunks = []
+    start = 0
+    length = len(text)
+
+    while start < length:
+        end = min(start + max_chars, length)
+        chunks.append(text[start:end])
+        start = end
+
+    return chunks
+    
+def summarize_long_text(text: str) -> str:
+    chunks = split_text(text)
+    summarized = []
+    summarized_text = ""
+
+    for _, chunk in enumerate(chunks):
+        summarized.append(summarize_text(chunk))
+
+    for i in range(len(summarized)):
+        summarized_text +=  summarized[i] 
+
+    return summarized_text if len(summarized) > 0 else "None"
+
--- Scholarly/routes.py
+++ Scholarly/routes.py
@@ -2,9 +2,12 @@ import secrets, os
 from flask import render_template, flash
 from flask_login import current_user, login_required, logout_user, login_user
 from Scholarly import app, db, bcrypt
+from Scholarly.models import User, Notes
+from Scholarly.forms import LoginForm, RegistrationForm, AccountForm
 from PIL import Image
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
