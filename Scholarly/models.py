fd8884ebb73e63793d1feff9f34554086f15195c
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-07 19:39:29 +0800

    Added the User and Notes model

---
+++ Scholarly/models.py
@@ -0,0 +1,18 @@
+from Scholarly import db, app, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    notes = db.relationship('Notes', backref='author', lazy=True)
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(20000), unique=True, nullable=False)
