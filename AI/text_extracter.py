7b181cc04c4540aeb4dbbcd9a545dc4b7e9fd610
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-10 20:46:55 +0800

    Went back to easyocr

--- AI/text_extracter.py
+++ AI/text_extracter.py
@@ -1,44 +1,24 @@
 import os
 from fitz import open as fitz_open
-from paddleocr import PaddleOCR
-from PIL import Image, ImageFilter, ImageOps
-from random import randint
+from easyocr import Reader
 
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
-
+reader = Reader(['en'], gpu=False)  
 
 supported_image_files = {'png', 'jpg', 'jpeg'}
-
-def preprocess_image(image_path):
-    image = Image.open(image_path)
-    """.convert("L")
-    image = ImageOps.invert(image)
-    image = image.point(lambda x: 0 if x < 140 else 255)  
-    image = image.filter(ImageFilter.SHARPEN)
-    image = image.resize((image.width * 2, image.height * 2))"""
-    return image.convert("RGB")  
-
 
 def is_supported_image_file(path: str) -> bool:
     ext = os.path.splitext(path)[1].lower().lstrip('.')
     return ext in supported_image_files
 
-def extract_handwritten_text(image_path):
-    results = ocr.ocr(image_path, cls=True)
-    extracted_text = []
-
-    for line in results[0]:
-        text = line[1][0]
-        extracted_text.append(text)
-
-    return "\n".join(extracted_text)
+def extract_handwritten_text(image_path: str) -> str:
+    result = reader.readtext(image_path, detail=0)
+    return "\n".join(result) if result else "No text found."
 
 def extract_pdf_text(pdf_path: str) -> str:
     doc = fitz_open(pdf_path)
     text = ""
     for page in doc:
         text += page.get_text()
-    print(text)
     return text.strip() if text else "No text found."
 
 def extract_text(path: str) -> str:
