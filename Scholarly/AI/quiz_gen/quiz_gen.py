78df8a3b322f47ad574d514c7cfa21f8088cc3dd
peejaykoy12345 <rick.well.rick@gmail.com>
2025-08-10 17:45:06 +0800

    Disabled Flan-t5

--- Scholarly/AI/quiz_gen/quiz_gen.py
+++ Scholarly/AI/quiz_gen/quiz_gen.py
@@ -1,4 +1,4 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+"""from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")  
 model = T5ForConditionalGeneration.from_pretrained("AI/models/checkpoint-64", use_safetensors=True)
@@ -9,4 +9,4 @@ input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 output_ids = model.generate(input_ids, max_length=128)
 output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
-print("🧠 Generated:", output_text)
+print("🧠 Generated:", output_text)"""
